fc4763d10f46b04152cbf426c98e5ae557e9cdb7
sgarc239 <37846917+sgarc239@users.noreply.github.com>
2018-03-27 14:32:24 -0400

    Uploaded Files to repo
    Uploaded GameFactory.cs and Game.cs C# classes.

---
+++ Game.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Game
+    {
+        public string team1 { get; set; }
+        public int team1score { get; set; }
+        public string team2 { get; set; }
+        public int team2score { get; set; }
+
+        public Game()
+        {
+
+        }
+
+        public Game(string team1, int team1score, string team2, int team2score)
+        {
+            this.team1 = team1;
+            this.team1score = team1score;
+            this.team2 = team2;
+            this.team2score = team2score;
+        }
+
+        public override string ToString()
+        {
+            return team1 + " (" + team1score + ") - " + team2 + " (" + team2score + ")";
+        }
+    }
+
+    
+}
---
+++ GameFactory.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace ClassLibrary1
+{
+    public class GameFactory
+    {
+        StreamWriter sw;
+        XmlSerializer serial;
+        List<Game> gameList;
+        const string GAMES_FILENAME = @"..\..\games.xml";
+
+        public void CreateGameList()
+        {
+            gameList = new List<Game>();
+
+            //Creating 6 games with 12 teams and populating their scores. Italian Serie A (Soccer)
+            Game game;
+            game = new Game("Juventus", 2, "Inter", 0);
+            gameList.Add(game);
+
+            game = new Game("Fiorentina", 1, "Napoli", 0);
+            gameList.Add(game);
+
+            game = new Game("Milan", 1, "Roma", 3);
+            gameList.Add(game);
+
+            game = new Game("Lazio", 0, "Sampdoria", 2);
+            gameList.Add(game);
+
+            game = new Game("Torino", 1, "Udinese", 1);
+            gameList.Add(game);
+
+            game = new Game("Atalanta", 2, "Bologna", 1);
+            gameList.Add(game);
+
+
+            serial = new XmlSerializer(gameList.GetType());
+            sw = new StreamWriter(GAMES_FILENAME);
+            serial.Serialize(sw, gameList);
+            sw.Close();
+        }
+
+        public List<Game> ReadGameFactory(out string resultMessage)
+        {
+            try
+            {
+                gameList = new List<Game>();
+                StreamReader sr = new StreamReader(GAMES_FILENAME);
+                serial = new XmlSerializer(gameList.GetType());
+                gameList = (List<Game>)serial.Deserialize(sr);
+                sr.Close();
+                resultMessage = "success";
+                return gameList;
+            }
+            catch (Exception ex )
+            {
+                resultMessage = "Exception thrown: " + ex.Message;
+                return null;
+            }
+        }
+    }
+
+
+}
